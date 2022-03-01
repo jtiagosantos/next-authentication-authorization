422234131782604e86f2a0dd9df300ae16ce8adf
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-02-28 22:34:55 -0400

    chore: configure axios

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from 'react';
+import { api } from '../services/axios/api';
 
 interface SignInCredentials {
   email: string;
@@ -20,7 +21,14 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const isAuthenticated = false;
 
   const signIn = async ({ email, password }: SignInCredentials) => {
-    console.log({ email, password });
+    try {
+      await api.post('sessions', {
+        email,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
