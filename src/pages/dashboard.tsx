875a8298a1903fb36864f68f23024dae9dbb9c61
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-03-01 08:43:54 -0400

    feat: refresh token #1

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
+import { api } from '../services/axios/api';
 
 const Dashboard = () => {
   const { user } = useAuth();
+
+  useEffect(() => {
+    api.get('/me').then((response) => console.log(response));
+  }, []);
 
   return <h1>Dashboard: {user?.email}</h1>;
 };
