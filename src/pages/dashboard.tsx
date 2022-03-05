7306ee2fc4ccfba3b102362343b923aa43ed8f6c
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-03-04 20:04:21 -0400

    feat: create permission hook

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
+import { useCan } from '../hooks/useCan';
 import { setupAPIClient } from '../services/axios/api';
 import { api } from '../services/axios/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
@@ -7,11 +8,20 @@ import { withSSRAuth } from '../utils/withSSRAuth';
 const Dashboard = () => {
   const { user } = useAuth();
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  });
+
   useEffect(() => {
     api.get('/me').then((response) => console.log(response));
   }, []);
 
-  return <h1>Dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 };
 
 export default Dashboard;
